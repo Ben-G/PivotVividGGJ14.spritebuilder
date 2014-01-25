bdfe7dbc832f761849144f6754ce9794c6595659
Ben-G <Benjamin.Encz@gmail.com>
2014-01-25 01:33:29 -0800

    added first moving enemy

---
+++ Source/BasicEnemy.h
@@ -0,0 +1,15 @@
+//
+//  BasicEnemy.h
+//  PivotVividGGJ14
+//
+//  Created by Benjamin Encz on 25/01/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface BasicEnemy : CCSprite
+
+@property (nonatomic, strong) NSString *moodToKill;
+
+@end
