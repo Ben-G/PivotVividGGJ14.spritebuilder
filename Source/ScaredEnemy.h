0d563368c0365a768a46b93367635d5a6e829aaf
Ben-G <Benjamin.Encz@gmail.com>
2014-01-26 06:33:02 -0800

    added scared enemy

---
+++ Source/ScaredEnemy.h
@@ -0,0 +1,13 @@
+//
+//  ScaredEnemy.h
+//  PivotVividGGJ14
+//
+//  Created by Benjamin Encz on 26/01/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Enemy.h"
+
+@interface ScaredEnemy : Enemy
+
+@end
