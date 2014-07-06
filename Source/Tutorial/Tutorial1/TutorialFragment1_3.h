6668d092355f8f82da8f7ff43a28c9f3a00b11b3
Ben-G <Benjamin.Encz@gmail.com>
2014-07-06 11:12:27 -0700

    improve tutorial

---
+++ Source/Tutorial/Tutorial1/TutorialFragment1_3.h
@@ -0,0 +1,13 @@
+//
+//  TutorialFragment1_3.h
+//  PivotVividGGJ14
+//
+//  Created by Benjamin Encz on 06/07/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "TutorialFragment.h"
+
+@interface TutorialFragment1_3 : TutorialFragment
+
+@end
--- Source/Tutorial/Tutorial1/TutorialFragment1_3.m
+++ Source/Tutorial/Tutorial1/TutorialFragment1_3.m
@@ -1,17 +1,17 @@
 //
-//  TutorialFragment1_2.m
+//  TutorialFragment1_3.m
 //  PivotVividGGJ14
 //
-//  Created by Benjamin Encz on 30/03/14.
+//  Created by Benjamin Encz on 06/07/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
-#import "TutorialFragment1_2.h"
+#import "TutorialFragment1_3.h"
 
-@implementation TutorialFragment1_2
+@implementation TutorialFragment1_3
 
 - (void)tutorialGameplayCompletedFragment:(TutorialGameplay *)tutorialGameplay {
-    [tutorialGameplay nextTutorialStep];
+    [tutorialGameplay winTutorial];
 }
 
 @end
